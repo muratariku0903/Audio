ecd3aa8cd5b8544ec214d56e9136c025aef2c16a
muratariku0903 <18ba179r@rikkyo.ac.jp>
2022-08-15 05:39:25 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+
+const SOUND_DATA = 'sounds/voice.m4a';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Sound App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  void onClick() async {
+    final player = AudioPlayer();
+    await player.setVolume(1);
+    await player.play(AssetSource(SOUND_DATA));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+              child: TextButton(
+                onPressed: () async {
+                  onClick();
+                },
+                child: const Text('Click!'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
